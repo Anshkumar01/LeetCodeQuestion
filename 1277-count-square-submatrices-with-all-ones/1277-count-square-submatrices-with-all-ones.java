4228efc460a0f2543bdcabf90d1cef27dee745ef
Ansh Kumar <132176164+Anshkumar01@users.noreply.github.com>
2025-08-20 20:25:44 +0530

    Time: 6 ms (75%), Space: 55.6 MB (29.78%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,37 +1,22 @@
 class Solution {
     public int countSquares(int[][] matrix) {
-        // Get dimensions of the matrix
-        int n = matrix.length;    // number of rows
-        int m = matrix[0].length; // number of columns
-        
-        // Create a DP table with same dimensions as matrix
-        int[][] dp = new int[n][m];
-        
-        // Variable to store total count of squares
-        int ans = 0;
-        
-        // Initialize first column of DP table
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = matrix[i][0];
-            ans += dp[i][0];
-        }
-        
-        // Initialize first row of DP table
-        for (int j = 1; j < m; j++) {
-            dp[0][j] = matrix[0][j];
-            ans += dp[0][j];
-        }
-        
-        // Fill the DP table for remaining cells
-        for(int i = 1; i < n; i++) {
-            for(int j = 1; j < m; j++) {
-                if(matrix[i][j] == 1) {
-                    dp[i][j] = 1 + Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]);
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int totalSquares = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+                    }
+                    totalSquares += dp[i][j];
                 }
-                ans += dp[i][j];
             }
         }
-        
-        return ans;
+        return totalSquares;
     }
 }
