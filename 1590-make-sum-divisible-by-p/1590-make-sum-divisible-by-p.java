36d0cf2708c4c6253e4320ba0baea0d69548c58d
Ansh Kumar <132176164+Anshkumar01@users.noreply.github.com>
2025-11-30 21:13:04 +0530

    Time: 32 ms (21.98%), Space: 98.1 MB (53.63%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.java
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.java
@@ -1,25 +1,28 @@
 class Solution {
     public int minSubarray(int[] nums, int p) {
-    int sum = 0;    
-    for(int x : nums)
-    sum = (sum + x)%p;
-  
-    int mod = sum%p; 
-    if(mod==0)
-    return 0;
+        long total = 0;
+        for (int num : nums) total += num;
 
-    int ans = nums.length;
-    HashMap<Integer, Integer>hm = new HashMap<>();
-    hm.put(0, -1);
-    int currSumMod = 0;
-    for(int i=0; i<nums.length; i++)
-    {
-      currSumMod = (currSumMod+nums[i])%p;
-      int neededMod = (currSumMod - mod + p)%p;
-      if(hm.containsKey(neededMod))
-      ans = Math.min(ans, i-hm.get(neededMod));
-      hm.put(currSumMod, i);
-    }
-     return ans==nums.length ? -1 : ans;
+        long target = total % p;
+        if (target == 0) return 0;
+
+        Map<Integer, Integer> mMap = new HashMap<>();
+        mMap.put(0, -1);
+
+        long prefix = 0;
+        int res = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            prefix = (prefix + nums[i]) % p;
+            int need = (int)((prefix - target + p) % p);
+
+            if (mMap.containsKey(need)) {
+                res = Math.min(res, i - mMap.get(need));
+            }
+
+            mMap.put((int)prefix, i);
+        }
+
+        return res == nums.length ? -1 : res;
     }
 }
